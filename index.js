6822261f1151bdfa3266294e9066e330cf03deeb
Scott Gress <scottmgress@gmail.com>
2015-02-20 16:35:04 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var mapping = require('./allowable-bad-characters');
+
+var convertAllowableBadCharsToEnglish = function(string) {
+      
+  var chars = string.split('');
+  return chars.map(function(char, index) {
+    if (mapping[char]) {
+      var transformed = '';
+      var previousChar = chars[index - 1];
+      var nextChar = chars[index + 1];
+      if (previousChar && previousChar != '_' && !mapping[previousChar]) {
+        transformed += '_';
+      }
+      transformed += mapping[char];
+      if (nextChar && nextChar != '_' && !mapping[nextChar]) {
+        transformed += '_';
+      }
+      return transformed;
+    } else {
+      return char;
+    }
+  }).join('');
+
+
+};
+
+convertAllowableBadCharsToEnglish.mapping = mapping;
+module.exports = convertAllowableBadCharsToEnglish;
